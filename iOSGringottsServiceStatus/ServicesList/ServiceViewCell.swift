ff964e451fa91f88ac08a4a6be980ab4201bb56d
Gardenia Mejia <dgma0607@gmail.com>
2024-02-23 15:52:15 -0600

    tableView added

---
+++ iOSGringottsServiceStatus/ServicesList/ServiceViewCell.swift
@@ -0,0 +1,97 @@
+//
+//  ServiceViewCell.swift
+//  iOSGringottsServiceStatus
+//
+//  Created by Gardenia Mejia on 23/02/24.
+//
+
+import UIKit
+
+protocol Reusable {
+    static var reuseIdentifier: String { get }
+}
+
+extension Reusable {
+    static var reuseIdentifier: String {
+        String(describing: self)
+    }
+}
+
+final class ServiceViewCell: UITableViewCell {
+    
+//    MARK: - Private properties
+    
+    private let nameLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .label
+        return label
+    }()
+    
+    private let stackStatus: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let iconStatus: UIImageView = {
+        let icon: UIImageView = UIImageView()
+        return icon
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        nil
+    }
+    
+    private func setupUI() {
+        selectionStyle = .none
+        contentView.addSubview(nameLabel)
+        stackStatus.addArrangedSubview(iconStatus)
+        stackStatus.addArrangedSubview(statusLabel)
+        contentView.addSubview(stackStatus)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            // stack
+            stackStatus.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackStatus.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stackStatus.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: stackStatus.leadingAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            iconStatus.heightAnchor.constraint(equalToConstant: 20.0),
+            iconStatus.widthAnchor.constraint(equalToConstant: 20.0)
+        ])
+    }
+    
+    func configCell(service: Service) {
+        nameLabel.text = service.route
+        iconStatus.image = UIImage(named: service.status.rawValue.lowercased())
+        statusLabel.text = "\(service.httpCode) - \(service.status.rawValue)"
+        
+        switch service.status {
+        case .active:
+            statusLabel.textColor = .systemGreen
+        case .inactive:
+            statusLabel.textColor = .systemRed
+        }
+    }
+}
+extension ServiceViewCell: Reusable {}
--- iOSGringottsServiceStatus/ViewController.swift
+++ iOSGringottsServiceStatus/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+    
+    private let mainStack: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    private let servicesList: ServicesListView = {
+        let tableView: ServicesListView = ServicesListView(frame: .zero, style: .insetGrouped)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupUI()
+        setConstraints()
     }
-
-
+    
+    private func setupUI() {
+        title = "HP - API"
+        view.backgroundColor = .systemBackground
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        mainStack.addArrangedSubview(servicesList)
+        view.addSubview(mainStack)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            mainStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mainStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mainStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+        ])
+    }
 }
 
