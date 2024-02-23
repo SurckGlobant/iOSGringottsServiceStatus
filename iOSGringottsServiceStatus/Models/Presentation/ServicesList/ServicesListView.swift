45432d0acd0ad18bcc01a3ac60e45ebb6833f3e8
Gardenia Mejia <dgma0607@gmail.com>
2024-02-23 17:03:21 -0600

    missing file added

--- iOSGringottsServiceStatus/Models/Presentation/ServicesList/ServicesListView.swift
+++ iOSGringottsServiceStatus/Models/Presentation/ServicesList/ServicesListView.swift
@@ -4,12 +4,33 @@
 //
 //  Created by Gardenia Mejia on 23/02/24.
 //
+//
+//  ServicesListView.swift
+//  iOSGringottsServiceStatus
+//
+//  Created by Gardenia Mejia on 23/02/24.
+//
 import UIKit
+
+enum Status: String {
+    case active = "Activo"
+    case inactive = "Inactivo"
+}
+
+struct Service {
+    let name: String
+    let route: String
+    let status: Status
+    let httpCode: String
+}
 
 final class ServicesListView: UITableView {
     
-    init() {
-        super.init(frame: .zero, style: .plain)
+    var data: [ServiceStatus] = []
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: .insetGrouped)
+        configTable()
     }
     
     required init?(coder: NSCoder) {
@@ -23,7 +44,30 @@ final class ServicesListView: UITableView {
         rowHeight = UITableView.automaticDimension
         estimatedRowHeight = 50.0
         allowsSelection = false
+        delegate = self
+        dataSource = self
         
-        register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
+        register(
+            ServiceViewCell.self,
+            forCellReuseIdentifier: ServiceViewCell.reuseIdentifier)
+    }
+}
+extension ServicesListView: UITableViewDelegate {}
+extension ServicesListView: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell: ServiceViewCell = tableView.dequeueReusableCell(withIdentifier: ServiceViewCell.reuseIdentifier, for: indexPath) as? ServiceViewCell
+        else {
+            return UITableViewCell()
+        }
+        cell.configCell(service: data[indexPath.row])
+        return cell
     }
 }
